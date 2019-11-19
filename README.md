b5cfd5ac25ecb60c00dcf26fba1cec56f0d631e5
Takashi Kitajima <inc@2inc.org>
2019-11-19 11:18:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Guten block
+
+## Get Started
+```
+$ npm install
+$ npm run build
+```
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: My Guten Block
+ */
+
+add_action(
+	'init',
+	function() {
+		$asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
+
+		wp_register_script(
+			'myguten-block',
+			plugins_url( 'build/index.js', __FILE__ ),
+			$asset_file['dependencies'],
+			$asset_file['version']
+		);
+
+		register_block_type(
+			'myguten-block/test-block',
+			[
+				'editor_script' => 'myguten-block',
+			]
+		);
+	}
+);
